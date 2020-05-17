6237f20468b99b414cdf0b1f3c9cfcca3fb4d476
Victor França <63361486+victorfclima@users.noreply.github.com>
2020-05-16 22:06:09 -0300

    Front-end and CSS finished

---
+++ scripts/controller/GetWeather.js
@@ -0,0 +1,70 @@
+class GetWeather {
+
+    constructor() {
+        this.inputCity = document.querySelector('#input-city').value
+
+        this.selectCity()
+        this.getApi()
+    }
+
+    selectCity() {
+        this.getApi(this.inputCity)
+    }
+
+    getApi(city) {
+        // Weather API sample javascript code
+        // Requires: jQuery and crypto-js (v3.1.9)
+        // 
+        // Copyright 2019 Oath Inc. Licensed under the terms of the zLib license see https://opensource.org/licenses/Zlib for terms.
+
+        var url = 'https://weather-ydn-yql.media.yahoo.com/forecastrss';
+        var method = 'GET';
+        var app_id = 'VJoMsa4i';
+        var consumer_key = 'dj0yJmk9YWdpYU05QmtzcVpRJmQ9WVdrOVZrcHZUWE5oTkdrbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWE1';
+        var consumer_secret = 'ab55495f3f199d993292cc30fcd2dd0759213298';
+        var concat = '&';
+        var query = { 'location': city, 'format': 'json' };
+        var oauth = {
+            'oauth_consumer_key': consumer_key,
+            'oauth_nonce': Math.random().toString(36).substring(2),
+            'oauth_signature_method': 'HMAC-SHA1',
+            'oauth_timestamp': parseInt(new Date().getTime() / 1000).toString(),
+            'oauth_version': '1.0'
+        };
+
+        var merged = {};
+        $.extend(merged, query, oauth);
+        // Note the sorting here is required
+        var merged_arr = Object.keys(merged).sort().map(function (k) {
+            return [k + '=' + encodeURIComponent(merged[k])];
+        });
+        var signature_base_str = method
+            + concat + encodeURIComponent(url)
+            + concat + encodeURIComponent(merged_arr.join(concat));
+
+        var composite_key = encodeURIComponent(consumer_secret) + concat;
+        var hash = CryptoJS.HmacSHA1(signature_base_str, composite_key);
+        var signature = hash.toString(CryptoJS.enc.Base64);
+
+        oauth['oauth_signature'] = signature;
+        var auth_header = 'OAuth ' + Object.keys(oauth).map(function (k) {
+            return [k + '="' + oauth[k] + '"'];
+        }).join(',');
+
+        $.ajax({
+            url: url + '?' + $.param(query),
+            headers: {
+                'Authorization': auth_header,
+                'X-Yahoo-App-Id': app_id
+            },
+            method: 'GET',
+            success: function (data) {
+                console.log(data);
+            }
+        });
+
+    }
+
+}
+
+
